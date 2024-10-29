4f8f04192b26e7e238c566e5367a6838b1e3ad89
Vivaldo Gaston <vivaldo2009@live.com.pt>
2024-10-29 17:40:38 +0100

    Limit jobs listing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 	import Navbar from "@/components/Navbar.vue";
 	import Hero from "@/components/Hero.vue";
+	import HomeCards from "@/components/HomeCards.vue";
+	import JobListings from "./components/JobListings.vue";
 </script>
 <template>
 	<Navbar />
 	<Hero title="Test Tile" subtitle="Test Subtitle" />
+	<HomeCards />
+	<JobListings :limit="3" show-button="true" />
 </template>
--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -1,16 +1,34 @@
-<script setup></script>
+<script setup>
+	import Card from "@/components/Card.vue";
+</script>
 <template>
-	<section class="bg-green-700 py-20 mb-4">
-		<div
-			class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-		>
-			<div class="text-center">
-				<h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-					Become a Vue Dev
-				</h1>
-				<p class="my-4 text-xl text-white">
-					Find the Vue job that fits your skills and needs
-				</p>
+	<section class="py-4">
+		<div class="container-xl lg:container m-auto">
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+				<Card>
+					<h2 class="text-2xl font-bold">For Developers</h2>
+					<p class="mt-2 mb-4">
+						Browse our Vue jobs and start your career today
+					</p>
+					<a
+						href="jobs.html"
+						class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+					>
+						Browse Jobs
+					</a>
+				</Card>
+				<Card bg="bg-green-100">
+					<h2 class="text-2xl font-bold">For Employers</h2>
+					<p class="mt-2 mb-4">
+						List your job to find the perfect developer for the role
+					</p>
+					<a
+						href="add-job.html"
+						class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
+					>
+						Add Job
+					</a>
+				</Card>
 			</div>
 		</div>
 	</section>
